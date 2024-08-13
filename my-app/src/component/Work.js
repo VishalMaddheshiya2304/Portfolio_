82ac9df0647e8f66740c0cedd198025a447bc35b
Vishal <vishalmaddheshiya864@gmail.com>
2024-08-13 09:13:50 +0530

    modifying home page adding skillsets section

--- my-app/src/component/Work.js
+++ my-app/src/component/Work.js
@@ -1,22 +1,145 @@
 import "./WorkCardStyles.css";
-import React from 'react';
-import prop1 from "../assets/prop1.png";
+import React from "react";
+import prop2 from "../assets/languages.png";
+import prop3 from "../assets/Frontend-Development.jpg";
+import prop4 from "../assets/Development tools.png";
+import Cplus from "../assets/c++.png";
+import javascript from "../assets/javascript.png";
+import java from "../assets/java.png";
+import python from "../assets/python.png";
+import SQL from "../assets/SQL.png";
+import C from "../assets/C.png";
+import HTML from "../assets/HTML.png";
+import CSS from "../assets/CSS.png";
+import Bootstrap from "../assets/Bootstrap.png";
+import VSCode from "../assets/VSCode.png";
+import Replit from "../assets/Replit.png";
+import Git from "../assets/Git.png";
+import GitHub from "../assets/GitHub.png";
+import IntelliJ from "../assets/IntelliJ.jpeg";
+import ReactLogo from "../assets/React.png"; 
 import { NavLink } from "react-router-dom";
 
 const Work = () => {
   return (
     <div className="work-container">
-      <h1 className="project-heading">Projects</h1>
+      <h1 className="project-heading">My Skillsets</h1>
       <div className="project-container">
         <div className="project-card">
-          <img src={prop1} alt="imge" />
-          <h2 className="project-title">Project-Title</h2>
+          <img src={prop2} alt="Languages" />
+          <h2 className="project-title">Languages</h2>
           <div className="pro-details">
-            <p>This is text</p>
-            <div className="pro-btns">
-              <NavLink to="url.com" className="btn">View</NavLink>
-              <NavLink to="url.com" className="btn">source</NavLink>
-            </div>
+            <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex', flexWrap: 'wrap' }}>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={Cplus} alt="C++ logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>C++</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={C} alt="C logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>C</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={javascript} alt="JavaScript logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>JavaScript</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={java} alt="Java logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>Java</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={python} alt="Python logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>Python</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={SQL} alt="SQL logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>SQL</figcaption>
+                </figure>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <img src={prop3} alt="Front-End Technologies" />
+          <h2 className="project-title">Front-End Technologies</h2>
+          <div className="pro-details">
+            <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex', flexWrap: 'wrap' }}>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={HTML} alt="HTML logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>HTML</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={CSS} alt="CSS logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>CSS</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={Bootstrap} alt="Bootstrap logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>Bootstrap</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={ReactLogo} alt="React logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>React</figcaption>
+                </figure>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <img src={prop4} alt="Development Tools" />
+          <h2 className="project-title">Development Tools</h2>
+          <div className="pro-details">
+            <ul style={{ listStyleType: 'none', padding: 0, margin: 0, display: 'flex', flexWrap: 'wrap' }}>
+              
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={Git} alt="Git logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>Git</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={GitHub} alt="GitHub logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>GitHub</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={VSCode} alt="VS Code logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>VS Code</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={IntelliJ} alt="IntelliJ IDEA logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>IntelliJ IDEA</figcaption>
+                </figure>
+              </li>
+              <li style={{ width: '50%', marginBottom: '20px', textAlign: 'center' }}>
+                <figure>
+                  <img src={Replit} alt="Replit logo" style={{ width: '48px', height: '48px' }} />
+                  <figcaption style={{ color: 'white' }}>Replit</figcaption>
+                </figure>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- my-app/src/routes/Project.js
+++ my-app/src/routes/Project.js
@@ -1,22 +1,49 @@
+
 import React from 'react';
+
 import Navbar from '../component/Navbar';
 import Footer from '../component/Footer';
 import Heroimg2 from '../component/Heroimg2';
 import WorkCard from '../component/WorkCard';
-
-
-
-
+import projectImage1 from '../assets/project1.jpg'; 
+import projectImage2 from '../assets/project2.jpg';
+import projectImage3 from '../assets/project3.jpg';
 
 const Project = () => {
   return (
     <div>
-       <Navbar/>
-       <Heroimg2 heading="PROJECTS." text="Some of my most recents works"/>
-       <WorkCard/>
-     <Footer/>
+      <Navbar />
+      <Heroimg2 heading="PROJECTS." text="Some of my most recent works" />
+      
+      <div className="project-section">
+        <WorkCard 
+          title="Project 1" 
+          image={projectImage1} 
+          description="Description of Project 1" 
+          viewLink="https://example.com/project1" 
+          sourceLink="https://github.com/VishalMaddheshiya2304/project1"
+        />
+
+        <WorkCard 
+          title="Project 2" 
+          image={projectImage2} 
+          description="Description of Project 2" 
+          viewLink="https://example.com/project2" 
+          sourceLink="https://github.com/VishalMaddheshiya2304/project2"
+        />
+
+        <WorkCard 
+          title="Project 3" 
+          image={projectImage3} 
+          description="Description of Project 3" 
+          viewLink="https://example.com/project3" 
+          sourceLink="https://github.com/VishalMaddheshiya2304/project3"
+        />
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 };
 
 export default Project;
